231ec40083a695c238fa95f16ef92650e8586865
Ernani12 <39600025+Ernani12@users.noreply.github.com>
2024-07-23 16:15:12 -0300

    ajustado login, logout e register

--- src/main/java/com/example/registration/config/SpringSecurity.java
+++ src/main/java/com/example/registration/config/SpringSecurity.java
@@ -28,7 +28,8 @@ public class SpringSecurity {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests((authorize) ->
-                        authorize.requestMatchers("/register/**").permitAll()
+                        authorize
+                                .requestMatchers("/register/**", "/index", "/login").permitAll()
                                 .requestMatchers("/index").permitAll()
                                 .requestMatchers("/users").hasRole("ADMIN")
                 ).formLogin(
